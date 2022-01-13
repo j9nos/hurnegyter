ef9f9126edb868ff95f4e5aaaf9da6805ae700b4
olahjg <k75681146744@ktch.hu>
2022-01-13 23:33:44 +0100

    x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+/*
+ * File: app.components.ts
+ * Author: Olah Janos Gergely
+ * Copyright: 2022, olajg
+ * Group: Szoft N/2
+ * Date: 2022-01-13
+ * Github: https://github.com/olahjg/
+ * Licenc: GNU GPL
+*/
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hurnegyter';
+  result: string = '...';
+  hurnegyterForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.#init();
+  }
+  #init() {
+    this.hurnegyterForm = this.formBuilder.group({
+      sideA: new FormControl(),
+      sideB: new FormControl(),
+      sideC: new FormControl(),
+      sideD: new FormControl(),
+    });
+  }
+  onSubmit() {
+    this.#calcArea(
+      this.hurnegyterForm.controls['sideA'].value,
+      this.hurnegyterForm.controls['sideB'].value,
+      this.hurnegyterForm.controls['sideC'].value,
+      this.hurnegyterForm.controls['sideD'].value
+    );
+  }
+  #calcArea(a: number, b: number, c: number, d: number): void {
+    var s: number = this.#calcHalfArea(a, b, c, d);
+    var T: number = Math.sqrt((s - a) * (s - b) * (s - c) * (s - d));
+    this.result = !isNaN(T) ? T.toFixed(3) : 'Nincs ilyen arány';
+  }
+  #calcHalfArea(a: number, b: number, c: number, d: number): number {
+    return (a + b + c + d) / 2;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+/*
+ * File: app.module.ts
+ * Author: Olah Janos Gergely
+ * Copyright: 2022, olajg
+ * Group: Szoft N/2
+ * Date: 2022-01-13
+ * Github: https://github.com/olahjg/
+ * Licenc: GNU GPL
+*/
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
